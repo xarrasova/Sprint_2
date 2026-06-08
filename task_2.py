23d3fd94d7d3489a7ef05f7f53d7de4d365be6d3
Харрасова Анна <anna.kharrasova@yandex.ru>
2026-06-08 19:12:28 +0400

    Исправление 2 задания. Спасибо!

--- task_2.py
+++ task_2.py
@@ -7,12 +7,12 @@ class Movies:
 
 class Comedy(Movies):
     def add_movie(self, movie):
-            self.movies.append(movie)
+            super().add_movie(movie)
             return f'Комедии: {self.movies}'
 
 class Drama(Movies):
     def add_movie(self, movie):
-            self.movies.append(movie)
+            super().add_movie(movie)
             return f'Драмы: {self.movies}'
 
 comedy = Comedy()
